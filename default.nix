ec0765da421e6fa43f65784b82fe9ff05f28c559
tobias pflug <tobias.pflug@gmail.com>
2019-09-07 17:15:33 +0200

    default.nix: only expose hie-core

--- default.nix
+++ default.nix
@@ -10,19 +10,18 @@ let
   callCabal2nix = pkgs.haskell.packages.${compiler}.callCabal2nix;
   doJailbreak = pkgs.haskell.lib.doJailbreak;
   dontCheck = pkgs.haskell.lib.dontCheck;
+
+  hie-bios = doJailbreak (callCabal2nix "hie-bios" "${bios}" {});
+
+  rope-utf16-splay = callCabal2nix "rope-utf16-splay" "${rope}" {};
+
+  lsp-types = doJailbreak (callCabal2nix "haskell-lsp" "${haskell-lsp}/haskell-lsp-types" {});
+
+  lsp = doJailbreak (callCabal2nix "haskell-lsp" "${haskell-lsp}" { haskell-lsp-types = lsp-types; rope-utf16-splay = rope-utf16-splay;});
+
 in
-  rec {
-    hie-bios = doJailbreak (callCabal2nix "hie-bios" "${bios}" {});
-
-    rope-utf16-splay = callCabal2nix "rope-utf16-splay" "${rope}" {};
-
-    lsp-types = doJailbreak (callCabal2nix "haskell-lsp" "${haskell-lsp}/haskell-lsp-types" {});
-
-    lsp = doJailbreak (callCabal2nix "haskell-lsp" "${haskell-lsp}" { haskell-lsp-types = lsp-types; rope-utf16-splay = rope-utf16-splay;});
-
-    hie-core = dontCheck (doJailbreak (callCabal2nix "hie-core" "${core}/compiler/hie-core" { 
-      inherit hie-bios; 
-      haskell-lsp = lsp; 
-      haskell-lsp-types = lsp-types;
-    }));
-  }
+  dontCheck (doJailbreak (callCabal2nix "hie-core" "${core}/compiler/hie-core" { 
+    inherit hie-bios; 
+    haskell-lsp = lsp; 
+    haskell-lsp-types = lsp-types;
+  }))
